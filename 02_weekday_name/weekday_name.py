1d2078b854f9ef508755b6e1b30f1f80b7a934d5
Daniel Bae <danibae91@icloud.com>
2021-03-28 00:10:34 -0700

    working on exercise

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -12,3 +12,10 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    # create list of DAYS
+    days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] 
+    # if statement (less than 1 & greater than 7)
+    if day_of_week < 1 and day_of_week > 7:
+        return None
+    # use index of list to return correct day -1 
+    return days[day_of_week -1]
