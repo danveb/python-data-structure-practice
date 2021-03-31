ae937f6baae23406ae40c70fd9eee02ea904fc8c
Daniel Bae <danibae91@icloud.com>
2021-03-31 00:49:17 -0700

    working on code

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,26 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    # # initialize empty list for duplicate_list
+    # duplicate_list = [] 
+    # # initialize a counter to count on number of duplicates
+    # counter = 0 
+    # # iterate for each_element in nums list 
+    # # if nums.count(each_element) 
+
+    # initialize counter at 0 
+    counter = 0
+    # num = nums[0] 
+    num = nums[0] 
+    # iterate val in nums 
+    for val in nums: 
+    # current_freq will be equal to nums.count(val) 
+        current_freq = nums.count(val) 
+    # if current_freq is > counter
+        if current_freq > counter: 
+    # counter is equal to current_freq 
+            counter = current_freq
+    # num = i 
+            num = val 
+    # return num 
+    return num 
